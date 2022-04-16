ec622e6e499257dc0b06edc0a4cc421a84cb216b
matsushinDB11 <matsushin35@gmail.com>
2022-04-17 03:33:57 +0900

    feat: setting eslint

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+    extraFileExtensions: ['.svelte'],
+  },
+  plugins: ['svelte3', '@typescript-eslint'],
+  overrides: [
+    {
+      files: ['*.svelte'],
+      processor: 'svelte3/svelte3',
+    },
+  ],
+  rules: {
+    // ...
+  },
+  settings: {
+    'svelte3/typescript': () => require('typescript'), // pass the TypeScript package to the Svelte plugin
+  },
+};
