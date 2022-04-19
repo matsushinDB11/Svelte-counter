e846c2c2ee5c00f4a9531546c89df465d04bf301
matsushinDB11 <matsushin35@gmail.com>
2022-04-19 18:16:06 +0900

    feat: create title list & sum of count

--- src/App.svelte
+++ src/App.svelte
@@ -2,35 +2,46 @@
   import { Button, Styles } from 'sveltestrap';
   import Counter from './components/counter.svelte';
 
-  // TODO この型いる？
   type TCounter = {
     id: number;
+    title: string;
+    count: number;
   };
 
-  let counterList: TCounter[] = [{ id: 0 }];
+  let counterList: TCounter[] = [{ id: 0, title: 'new counter', count: 0 }];
   let nextId = 1;
 
   function add(): void {
-    counterList = counterList.concat({ id: nextId });
+    counterList = counterList.concat({ id: nextId, title: 'new counter', count: undefined });
     nextId += 1;
   }
 
   function remove(event: CustomEvent<{ id: number }>): void {
     counterList = counterList.filter((counterList) => counterList.id != event.detail.id);
   }
+
+  function sumOfCounter(CounterList: TCounter[]): number {
+    return CounterList.map((counter) => counter.count).reduce((prev, curr) => prev + curr, 0);
+  }
 </script>
 
 <Styles />
-
 <main>
   <h1>Svelte Counter App</h1>
   {#each counterList as counter (counter.id)}
     <div>
-      <Counter id={counter.id} on:remove={remove} />
+      <Counter id={counter.id} on:remove={remove} bind:title={counter.title} bind:count={counter.count} />
     </div>
   {/each}
 
   <Button block color="info" on:click={add}>new counter</Button>
+  <p>
+    title list:
+    {#each counterList as counter}
+      {counter.title},
+    {/each}
+  </p>
+  <p>sum of count: {sumOfCounter(counterList)}</p>
 </main>
 
 <style>
