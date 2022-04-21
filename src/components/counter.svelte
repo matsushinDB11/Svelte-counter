8a0b5c3d9c9ef1109c38ac809d0ea41d685974a9
Shintaro Matsumoto <61864556+matsushinDB11@users.noreply.github.com>
2022-04-21 15:36:47 +0900

    課題の仕様に沿って実装 (#2)
    * create nvm setting file
    
    * fix: add @rollup/plugin-typescript
    
    * feat: add sveltstrap
    
    * feat: create counter component
    
    * feat: create counter func
    
    * feat: create couter list & operation func
    
    * feat: create add button
    
    * feat: create dipathcer to remove counter
    
    * feat: create title list & sum of count
    
    * fix: card style
    
    * feat: Update README
    
    * remove md file from lint-staged
    
    * feat: Add apmlify build settings
    
    * fix: add yarn to dev requirements

---
+++ src/components/counter.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import { Button, Card, CardBody, Input } from 'sveltestrap';
+  import { createEventDispatcher } from 'svelte';
+
+  export let id: number;
+  export let title = '';
+  export let count = 0;
+  function incrementCount(): void {
+    count += 1;
+  }
+  function decrementCount(): void {
+    if (count > 0) count -= 1;
+  }
+  function reset(): void {
+    count = 0;
+  }
+  const removeDispatcher = createEventDispatcher<{ remove: { id: number } }>();
+
+  function remove(): void {
+    removeDispatcher('remove', { id: id });
+  }
+</script>
+
+<Card>
+  <CardBody>
+    <Input style="width: 400px" bind:value={title} placeholder="input counter title" />
+    <span>{count}</span>
+    <Button color="success" on:click={incrementCount}>+</Button>
+    <Button color="danger" on:click={decrementCount}>-</Button>
+    <Button color="warning" on:click={reset}>0</Button>
+    <Button color="light" on:click={remove}>x</Button>
+  </CardBody>
+</Card>
